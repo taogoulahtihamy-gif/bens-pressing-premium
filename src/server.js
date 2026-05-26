a6f6dfe8012c2ce5f6b2f371e23c4da143d15a0f
EZO <taogoulahtihamy@gmail.com>
2026-05-26 14:50:38 +0000

    remove localhost references

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ import app from "./app.js"
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`🚀 Ben's Pressing API démarrée sur http://localhost:${PORT}`)
+  console.log(`🚀 Ben's Pressing API démarrée sur le port ${PORT}`)
 })
